e977dc1fc86127974318eaf067126cd555fe3681
ricctram <ricctram@gmail.com>
2024-04-29 20:59:17 +0200

    index

---
+++ app/Views/index.php
@@ -0,0 +1,29 @@
+<?= $this->extend('layout') ?>
+
+<?= $this->section('content') ?>
+<!-- CONTENT -->
+
+<section>
+
+<h1>Tesi di laurea</h1>
+
+    <pre><code>app/Views/welcome_message.php</code></pre>
+
+    <p>The corresponding controller for this page can be found at:</p>
+
+    <pre><code>app/Controllers/Home.php</code></pre>
+
+</section>
+
+<div>
+
+    <section>        
+
+        <h2>
+            Titolo 2
+        </h2>
+
+    </section>
+
+</div>
+<?= $this->endSection() ?>
