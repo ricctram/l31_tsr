97922908fc5f55218a70bedb149f9d01cbfc91af
ricctram <ricctram@gmail.com>
2024-05-01 19:36:23 +0200

    fix store e update controller menu

--- app/Controllers/Menu.php
+++ app/Controllers/Menu.php
@@ -16,6 +16,7 @@ class Menu extends BaseController
         $this->ionAuth = new \IonAuth\Libraries\IonAuth();
     }
 
+    
     /**
      * Mostra la lista dei menu.
      */
@@ -27,6 +28,8 @@ class Menu extends BaseController
         return view('menu/index', $data);
     }
 
+    
+    
     /**
      * Mostra il form per creare un nuovo menu.
      */
@@ -40,27 +43,48 @@ class Menu extends BaseController
         return view('menu/create', $data);
     }
 
+    
+    
     /**
      * Salva un nuovo menu nel database.
      */
     public function store()
     {
+        $data['ionAuth'] = $this->ionAuth;
+        
+        // Verifica se sono stati inviati dati dal form
+        if ($this->request->getMethod() === 'POST') {
+            // Recupera i dati inviati dal form
+            $menuData = [
+                'menu_name' => $this->request->getPost('menu_name'),
+                'description' => $this->request->getPost('description'),
+                'price_per_person' => $this->request->getPost('price_per_person'),
+            ];
 
-        // Recupera i dati del form
-        $data = [
-            //'event_id' => $this->request->getPost('event_id'),
-            'menu_name' => $this->request->getPost('menu_name'),
-            'description' => $this->request->getPost('description'),
-            'price_per_person' => $this->request->getPost('price_per_person'),
-        ];
+            // Salva il nuovo menu nel database
+            $menuID = $this->menuModel->createMenu($menuData);
 
-        // Salva il nuovo menu
-        $this->menuModel->createMenu($data);
+            // Associa gli elementi selezionati al menu appena creato
+            $selectedItems = $this->request->getPost('menu_items');
+            if (!empty($selectedItems)) {
+                foreach ($selectedItems as $itemID) {
+                    $this->menuModel->associateMenuItem($menuID, $itemID);
+                }
+            }
 
-        return redirect()->to('/menu')->with('success', 'Menu creato con successo.');
+            // Redirect alla pagina di successo o mostra un messaggio di successo
+            return redirect()->to(site_url('/menu'))->with('success', 'Menu creato con successo.');
+        }
 
+        // Se non sono stati inviati dati, visualizza il form di creazione del menu
+        // Recupera tutti gli elementi del menu per visualizzarli nel form
+        $data['menu_items'] = $this->menuModel->getMenuItems();
+
+        // Carica la vista del form di creazione del menu
+        return view('menu/create', $data);
     }
-
+    
+    
     /**
      * Mostra il form per modificare un menu.
      */
@@ -69,11 +93,14 @@ class Menu extends BaseController
         $data['ionAuth'] = $this->ionAuth;
 
         $data['menu'] = $this->menuModel->find($id);
+        $data['menu_items'] = $this->menuModel->getMenuItems();
+        $data['menu_selected_items'] = $this->menuModel->getSelectedMenuItems($id);
 
         return view('menu/edit', $data);
         
     }
-
+    
+    
     /**
      * Aggiorna un menu nel database.
      */
@@ -86,14 +113,17 @@ class Menu extends BaseController
             'description' => $this->request->getPost('description'),
             'price_per_person' => $this->request->getPost('price_per_person'),
         ];
+        $menuItemsData = $this->request->getPost('menu_items');
 
         // Aggiorna il menu
-        $this->menuModel->update($id, $data);
+        $this->menuModel->updateMenu($id, $data, $menuItemsData);
 
         return redirect()->to('/menu')->with('success', 'Menu aggiornato con successo.');
 
     }
 
+    
+    
     /**
      * Elimina un menu dal database.
      */
@@ -124,7 +154,8 @@ class Menu extends BaseController
         $data['menu_items'] = $this->menuModel->getMenuItems();
         return view('menu/menu_item/index', $data);
     
-    }
+    }    
+    
     
     public function createMenuItem() {
         
@@ -132,7 +163,8 @@ class Menu extends BaseController
         
         return view('menu/menu_item/create', $data);
     
-    }
+    }    
+    
     
     public function storeMenuItem() {
         // Recupera i dati del form
@@ -146,7 +178,8 @@ class Menu extends BaseController
         $this->menuModel->createMenuItem($data);
 
         return redirect()->to('/menu_item')->with('success', 'Piatto creato con successo.');
-    }
+    }    
+    
     
     public function editMenuItem($id) {
 
@@ -164,7 +197,8 @@ class Menu extends BaseController
             // Se il menu item non esiste, gestisci l'errore o reindirizza
             return redirect()->back()->with('error', 'Il menu item specificato non esiste.');
         }
-    }
+    }    
+    
     
     public function updateMenuItem($id) {
 
@@ -190,7 +224,8 @@ class Menu extends BaseController
         }
 
     }
-
+    
+    
     public function deleteMenuItem($id)
     {
         // Chiama il metodo deleteMenuItem() nel modello per eliminare il menu item
